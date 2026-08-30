027e2c3ec13d4af3cafb8fb1f3162f7041b50060
Mojtaba Zakeri <mojtabaza82@gmail.com>
2026-08-30 13:33:16 +0330

    Core Directory

---
+++ core/config.py
@@ -0,0 +1,7 @@
+# Here there are configs of project that you want to change
+
+IFACE = 'wlan1'
+DEAUTH_COUNT = 10
+WORDLIST = "wordlist.txt"
+AIRCRACK_TIMEOUT = 120
+DB_PATH = 'database/mydatabase.db'
---
+++ core/wifi_scanner.py
@@ -0,0 +1,24 @@
+from core.config import IFACE
+import re
+import pandas as pd
+import subprocess
+
+def scanning():
+    wifi_str = subprocess.getoutput(f"nmcli -t -f IN-USE,BSSID,SSID,CHAN,FREQ,SIGNAL,BARS,SECURITY device wifi list ifname {IFACE}")
+    rows = []
+    for line in wifi_str.strip().split("\n"):
+        parts = re.split(r"(?<!\\):",line)
+        if len(parts) == 8:
+            rows.append({
+            "IN-USE": parts[0],
+            "BSSID": parts[1].replace("\\:",":"),
+            "SSID": parts[2],
+            "CHAN": int(parts[3]) if parts[3].strip().isdigit() else None,
+            "FREQ": int(parts[4].replace("MHz","").strip()) if parts[4].replace("MHz", "").strip().isdigit() else None,
+            "SIGNAL": int(parts[5]) if parts[5].strip().isdigit() else None,
+            "BARS": parts[6],
+            "SECURITY": parts[7] if parts[7].strip() else "OPEN"
+            })
+    
+    wifi_table = pd.DataFrame(rows)
+    return wifi_table
