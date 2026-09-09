9b8cd900fc7f4b1197859d9a539468ab9c8cd211
Mojtaba Zakeri <mojtabaza82@gmail.com>
2026-09-09 20:24:16 +0330

    Config File

--- core/config.py
+++ core/config.py
@@ -1,6 +1,10 @@
 # Here there are configs of project that you want to change
 
 IFACE = 'wlan1'
+GPS_PORT = '/dev/serial0'
+GPS_BAUDRATE = 9600
+GPS_TIMEOUT = 1
+GPS_MAX_WAIT = 15
 DEAUTH_COUNT = 10
 WORDLIST = "wordlist.txt"
 AIRCRACK_TIMEOUT = 120
